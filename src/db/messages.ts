70ea99aae7d862bc05140d933e56192776d48568
Luiz Sócrate <luiz@socrate.com.br>
2023-01-22 20:42:53 +0100

    message endpoints

--- src/db/messages.ts
+++ src/db/messages.ts
@@ -1,5 +1,12 @@
 import { prisma } from "../prisma.js";
 
+const markAsFetched = async (ids: number[]) => {
+  await prisma.message.updateMany({
+    data: { fetchedAt: new Date() },
+    where: { id: { in: ids } },
+  });
+};
+
 export const createMessage = async (params: {
   content: string;
   recipient: string;
@@ -12,4 +19,32 @@ export const createMessage = async (params: {
   });
 
   return newMessage;
+};
+
+export const deleteMessages = async (ids: number[]) => {
+  await prisma.message.deleteMany({
+    where: {
+      id: {
+        in: ids,
+      },
+    },
+  });
+};
+
+export const fetchUnreadMessages = async () => {
+  const messages = await prisma.message.findMany({
+    where: { fetchedAt: { equals: null } },
+  });
+  await markAsFetched(messages.map((msg) => msg.id));
+  return messages;
+};
+
+export const fetchPaginated = async (start: number, stop: number) => {
+  const messages = await prisma.message.findMany({
+    orderBy: { createdAt: "asc" },
+    skip: start,
+    take: stop - start,
+  });
+  await markAsFetched(messages.map((msg) => msg.id));
+  return messages;
 };
--- src/routes/messages/routes.ts
+++ src/routes/messages/routes.ts
@@ -1,10 +1,15 @@
 import type { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
-import { createMessage } from "../../db/messages.js";
+import { z } from "zod";
+import * as Messages from "../../db/messages.js";
 
 export const messageRoutes: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ) => {
   fastify.register(postMessage);
+  fastify.register(getUnread);
+  fastify.register(getPaginated);
+  fastify.register(deleteMessage);
+  fastify.register(deleteQuery);
 };
 
 const postMessage: FastifyPluginAsyncJsonSchemaToTs = async (fastify) => {
@@ -31,7 +36,107 @@ const postMessage: FastifyPluginAsyncJsonSchemaToTs = async (fastify) => {
   } as const;
 
   fastify.post("/", { schema }, async (req, reply) => {
-    const newMessage = await createMessage(req.body);
+    const newMessage = await Messages.createMessage(req.body);
     reply.code(201).send(newMessage);
+  });
+};
+
+const getUnread: FastifyPluginAsyncJsonSchemaToTs = async (fastify) => {
+  const schema = {
+    response: {
+      200: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            id: { type: "number" },
+            recipient: { type: "string" },
+            content: { type: "string" },
+          },
+        },
+      },
+    },
+  } as const;
+  fastify.get("/unread", { schema }, async (req, reply) => {
+    const messages = await Messages.fetchUnreadMessages();
+    reply.code(200).send(messages);
+  });
+};
+
+const getPaginated: FastifyPluginAsyncJsonSchemaToTs = async (fastify) => {
+  const schema = {
+    querystring: {
+      type: "object",
+      required: ["start", "stop"],
+      properties: {
+        start: { type: "integer" },
+        stop: { type: "integer" },
+      },
+    },
+    response: {
+      200: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            id: { type: "number" },
+            recipient: { type: "string" },
+            content: { type: "string" },
+          },
+        },
+      },
+    },
+  } as const;
+  fastify.get("/paginated", { schema }, async (req, reply) => {
+    if (req.query.start > req.query.stop) {
+      return reply.code(400).send();
+    }
+
+    const messages = await Messages.fetchPaginated(
+      req.query.start,
+      req.query.stop
+    );
+    reply.code(200).send(messages);
+  });
+};
+
+const deleteMessage: FastifyPluginAsyncJsonSchemaToTs = async (fastify) => {
+  const schema = {
+    params: {
+      type: "object",
+      required: ["id"],
+      properties: {
+        id: { type: "integer" },
+      },
+    },
+  } as const;
+
+  fastify.delete("/:id", { schema }, async (req, reply) => {
+    await Messages.deleteMessages([req.params.id]);
+    reply.code(204).send();
+  });
+};
+
+const deleteQuery: FastifyPluginAsyncJsonSchemaToTs = async (fastify) => {
+  const schema = {
+    querystring: {
+      type: "object",
+      required: ["ids"],
+      properties: {
+        ids: { type: "string" },
+      },
+    },
+  } as const;
+
+  fastify.delete("/", { schema }, async (req, reply) => {
+    try {
+      const parsedIds = req.query.ids
+        .split(",")
+        .map((str) => z.coerce.number().parse(str));
+      await Messages.deleteMessages(parsedIds);
+      reply.code(204).send();
+    } catch (error) {
+      reply.code(400).send();
+    }
   });
 };
